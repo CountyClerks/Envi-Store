93c9b2b35460e09fb9ee333fe5da11253347972d
Eric Hardy <erichardy011@gmail.com>
2023-02-04 19:52:25 -0600

    init store project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  // const fakeData = fetch("https://fakestoreapi.com/products/").then(res => res.json()).then(json => console.log(json)) 20 PRODUCTS IN TOTAL
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
